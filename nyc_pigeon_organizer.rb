99248e09ad094201699294f6ff669cac9f0a6954
Mathew E Newton <elijah2895@hotmail.com>
2021-02-15 15:55:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value), new_array|
+    value.each do |first_key, names|
+      names.each do |name|
+        if !new_array[name]
+          new_array[name] = {}
+        end
+        if !new_array[name][key]
+          !new_array[name][key] = []
+        end
+        new_array[name][key] << first_key.to_s
+      end
+    end
+  end
 end
