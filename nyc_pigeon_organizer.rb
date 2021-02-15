4b7e36d2644942e94907ad80e8a449f0ba88cd5a
Mathew E Newton <elijah2895@hotmail.com>
2021-02-15 15:20:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), new_array|
+    value.each do |first_key, names|
     new_array
   end
   binding.pry
